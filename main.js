02a8e42bee75936ce97fae29a3966cd2f1364278
Fahmid Anabi <me.anabi@gmail.com>
2019-12-23 01:52:08 +0600

    init

---
+++ main.js
@@ -0,0 +1,48 @@
+const snake = document.querySelector(".snake");
+let x = 0;
+let y = 0;
+const speed = 50;
+let flipped = true;
+let rotate = 0;
+
+
+function handleKeyDown(event) {
+  // if its not an arrow key, we dont care
+  if (!event.key.includes("Arrow")) {
+    return;
+  }
+  switch (event.key) {
+    case "ArrowUp":
+      y = y - 1;
+      rotate = -90;
+      break;
+    case "ArrowDown":
+      y = y + 1;
+      rotate = 90;
+      break;
+    case "ArrowLeft":
+      x = x - 1;
+      rotate = 0;
+      flipped = false;
+      break;
+    case "ArrowRight":
+      x = x + 1;
+      rotate = 0;
+      flipped = true;
+      break;
+    default:
+      console.log("That is not a valid move");
+      break;
+  }
+
+  snake.setAttribute(
+    "style",
+    `
+        --rotateX: ${flipped ? "180deg" : "0"};
+        --x: ${x * speed}px;
+        --y: ${y * speed}px;
+        --rotate: ${rotate}deg;
+      `
+  );
+}
+window.addEventListener("keydown", handleKeyDown);
